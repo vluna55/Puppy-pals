c7ca377a9691bfdc46379fd5cc9c47e05ca7f86b
vluna55 <vincentluna70@yahoo.com>
2024-01-19 17:24:14 -0600

    Mapped through the puppy array

---
+++ puppy/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import { puppyList } from "./data.js";
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+
+  console.log("puppyList: ", puppyList);
+
+  return (
+    <>
+      <div>
+        {puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default App;
